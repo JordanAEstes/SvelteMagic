4d6e2b6d14ce2c0e0bf92f08c3f9ed739835f385
Jordan Estes <jordan.estes@gmail.com>
2022-04-12 11:04:38 -0500

    initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+	import logo from '../static/magic_logo.svg';
+</script>
+
+<header>
+	<h1>
+		<img src={logo} alt="magic logo" />
+	</h1>
+</header>
+
+<style>
+	header {
+		background: #6a2b1014;
+		padding: 20px;
+	}
+	h1 {
+		margin: 0;
+		text-align: center;
+	}
+	img {
+		max-width: 400px;
+	}
+</style>
